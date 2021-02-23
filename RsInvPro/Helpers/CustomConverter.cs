8d1b848bdfc7a457c8be8633ffe5d940b5699d25
jbaird <jbaird@outlook.com>
2021-02-23 08:18:35 -0500

    * GridConverter.cs: * CustomConverter.cs: RsInvPro/Views/InventoryView.xaml
    * InventoryView.xaml: add grid behaviors for selected item

---
+++ RsInvPro/Helpers/CustomConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace RsInvPro.Helpers
+{
+
+    public class GridConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return value as Syncfusion.SfDataGrid.XForms.GridSelectionChangedEventArgs;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
