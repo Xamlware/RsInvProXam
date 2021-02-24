b1be777d4b9c5c3ed0527c7c1213e6eceecafeab
jbaird <jbaird@outlook.com>
2021-02-24 15:42:08 -0500

    Add handler for data form autogenerate items

--- RsInvPro/Views/InventoryEditView.xaml.cs
+++ RsInvPro/Views/InventoryEditView.xaml.cs
@@ -4,6 +4,7 @@ using GalaSoft.MvvmLight.Ioc;
 using GalaSoft.MvvmLight.Messaging;
 using RsInvPro.Messages;
 using RsInvPro.ViewModels;
+using Syncfusion.XForms.DataForm;
 using Xamarin.Forms;
 
 namespace RsInvPro.Views
@@ -19,6 +20,10 @@ namespace RsInvPro.Views
             _ivm = SimpleIoc.Default.GetInstance<InventoryViewModel>();
         }
 
-      
+        private void DataForm_AutoGeneratingDataFormItem(object sender, AutoGeneratingDataFormItemEventArgs e)
+        {
+            if (e.DataFormItem.Name == "MiddleName")
+                e.Cancel = true;
+        }
     }
 }
